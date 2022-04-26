585c16e35e5c69ef62609325e33e4735a8cd4b3b
aradhyahs <42295752+aradhyahs@users.noreply.github.com>
2022-04-26 10:09:44 +0530

    Add files via upload

---
+++ TestAS400Connectivity.sh
@@ -0,0 +1,23 @@
+
+if [ "$1" == "-h" ]; then
+  echo "Usage:"
+  echo "For example"
+  echo "./TestAS400Connectivity.sh 9.51.99.163 QSECOFR SERVICE1 /QSYS.LIB/SANOVI.LIB/DGREPINFO.PGM MIMIX,AGNAME 10,10,10"
+  exit 0
+fi
+
+
+
+RUNTIME=$JAVA_HOME/bin/java
+
+CLASSPATH=/opt/AS400Jar/jt400.jar:/opt/AS400Jar/AS400ProgramCallHelper.jar
+
+MAIN=panaces.custom.lib.AS400ProgramCallHelper
+
+#export $CLASSPATH
+
+echo $CLASSPATH
+
+$RUNTIME -classpath ${CLASSPATH} $MAIN  $1 $2 $3 $4 $5 $6 
+
+
